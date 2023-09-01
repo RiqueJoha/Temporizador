cddd9d7e69355db45406ebaa26e7aa82b331fe0a
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-09-01 11:03:29 -0300

    corrección e inicio del temporizador

--- tmp.js
+++ tmp.js
@@ -3,36 +3,24 @@ const inputSeg=document.querySelector("#segundos");
 const inputMin=document.querySelector("#minutos");
 const inputHs=document.querySelector("#horas");
 
+let segundos=0;
+
 function iniciar() {
-    let horas = 0;
-    let minutos = 0;
-    let segundos = 0;
-    const intervalo = 1000; 
-
    
+    segundos = inputSeg.value;
+    minutos= inputMin.value;
+    horas=inputHs.value;
 
-    function mostrarTiempo() {
-      segundos++;
-      if (segundos === 60) {
-        segundos = 0;
-        minutos++;
-        if (minutos === 60) {
-          minutos = 0;
-          horas++;
-        }
+    
+
+    const idInterval = setInterval(() => {
+      inputResultado.value=`${horas}:${minutos}:${segundos}`
+      segundos--;
+      if (segundos<0) {
+        clearInterval(idInterval);
       }
-     inputResultado.value=`${horas}:${minutos}:${segundos}`;
-     inputSeg.value=`${segundos} Segundos`
-     inputMin.value=`${minutos} Minutos`
-     inputHs.value=`${horas} Horas`
-    }
+    }, 1000);
+
     
-   
-    const temporizador = setInterval(mostrarTiempo, intervalo);
-    
-    
-    setTimeout(() => {
-      clearInterval(temporizador);
-      inputResultado.value="Temporizador detenido.";
-    }, 5 * 60 * 1000); // Detener después de 5 minutos (5 * 60 * 1000 ms)
-}
+
+};
